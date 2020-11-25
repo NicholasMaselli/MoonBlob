6e5d91838360861878f3199f9787137c9a2a8387
Nicholas Maselli <maselli.nicholas@gmail.com>
2020-11-25 18:49:27 -0500

    Move Bob

---
+++ MoonBlob/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Inputs")]
+    private float horizontal;
+    private float vertical;
+
+    [Header("Physics Variables")]
+    public float walkSpeed = 6.0f;
+    private Vector3 velocity;
+
+    private void Update()
+    {
+        GetPlayerInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        CalculateVelocity();
+        transform.Translate(velocity, Space.World);
+    }
+
+    private void GetPlayerInputs()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void CalculateVelocity()
+    {
+        velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * walkSpeed;
+    }
+}
