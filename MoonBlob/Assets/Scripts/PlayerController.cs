371b6d8f12af84602a09b2d4a3c2d6505e146da3
Nicholas Maselli <maselli.nicholas@gmail.com>
2020-11-26 21:25:55 -0500

    Double Tap Dash Logic but its bad

--- MoonBlob/Assets/Scripts/PlayerController.cs
+++ MoonBlob/Assets/Scripts/PlayerController.cs
@@ -16,7 +16,10 @@ public class PlayerController : MonoBehaviour
     private bool jumping;
     private bool sprinting;
 
+    private bool dashHorizontal;
+
     [Header("Movement Variables")]
+    private Vector3 velocity;
     public float speed;
 
     [Header("Rotation Variables")]
@@ -24,8 +27,25 @@ public class PlayerController : MonoBehaviour
     private float sensitivity = 200f;
 
     [Header("Jump Variables")]
-    private bool readyToJump = true;
     public float jumpForce = 500f;
+
+    [Header("Grounded Variables")]
+    private bool grounded = true;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    [Header("Dash Variables")]
+    private bool dashing = false;
+    private bool readyToDash = true;
+    public float dashCooldown = 1;
+    public float dashSpeed = 5;
+
+    [Header("Button Cooldowns")]
+    public float buttonCooldown = 0.5f;
+    private float buttonTapCooldown;
+    private float buttonTapCount;
+    private string tapButton;
 
     //-----------------------------------------------------------------------------------//
     //PlayerController Initialization and Update
@@ -54,6 +74,57 @@ public class PlayerController : MonoBehaviour
         mouseHorizontal = Input.GetAxis("Mouse X");
         mouseVertical = Input.GetAxis("Mouse Y");
         jumping = Input.GetButton("Jump");
+
+        // Double Tap Dash Logic
+        string buttonDown = "-";
+        bool horizontal = Input.GetButtonDown("Horizontal");
+        bool vertical = Input.GetButtonDown("Vertical");
+
+        if (horizontal && x > 0)
+        {
+            buttonDown = "Dash Right";
+        }
+        else if (horizontal && x < 0)
+        {
+            buttonDown = "Dash Left";
+        }
+        else if (vertical && y > 0)
+        {
+            buttonDown = "Dash Forward";
+        }
+        else if (vertical && y < 0)
+        {
+            buttonDown = "Dash Backward";
+        }
+
+        if (buttonDown == tapButton)
+        {
+            if (buttonTapCooldown > 0 && buttonTapCount == 1)
+            {
+                dashing = true;
+                buttonTapCooldown = 0.5f;
+                buttonTapCount = 0;
+            }
+            else
+            {
+                buttonTapCooldown = 0.5f;
+                buttonTapCount += 1;
+            }
+
+            if (buttonTapCooldown > 0)
+            {
+                buttonTapCooldown -= 1 * Time.deltaTime;
+            }
+            else
+            {
+                buttonTapCount = 0;
+                tapButton = String.Empty;
+            }
+        }
+        else if (buttonDown != "-")
+        {
+            tapButton = buttonDown;
+        }
     }
     //-----------------------------------------------------------------------------------//
 
@@ -62,28 +133,74 @@ public class PlayerController : MonoBehaviour
     //-----------------------------------------------------------------------------------//
     private void Move()
     {
-        if (readyToJump && jumping)
+        if (jumping)
         {
             Jump();
         }
 
-        //Apply move forces
-        //playerRigidBody.AddForce(transform.forward * y * speed * Time.fixedDeltaTime);
-        //playerRigidBody.AddForce(transform.right * x * speed * Time.fixedDeltaTime);
+        CalculateVelocity();
 
-        // Apply Velocity to the rigid body while also handling collisions
-        Vector3 direction = (transform.forward * y) + (transform.right * x);
-        Vector3 velocity = direction * speed * Time.fixedDeltaTime;
+        // Applies velocity movement while also handling collisions
         playerRigidBody.MovePosition(transform.position + velocity);
     }
 
     private void Jump()
     {
-        //readyToJump = false;
+        if (!grounded) return;
+        
+        grounded = false;
 
         //Apply jump forces
         playerRigidBody.AddForce(Vector3.up * jumpForce * 1.5f);
-        jumping = false;
+        jumping = false;   
+    }
+
+    private Vector3 Dash()
+    {
+        if (!dashing || !readyToDash) return Vector3.zero;
+        readyToDash = false;
+        dashing = false;
+
+        Vector3 dashVector = Vector3.zero;
+        if (tapButton == "Dash Right")
+        {
+            dashVector = transform.right * dashSpeed;
+        }
+        else if (tapButton == "Dash Left")
+        {
+            dashVector = -transform.right * dashSpeed;
+        }
+        else if (tapButton == "Dash Forward")
+        {
+            dashVector = transform.forward * dashSpeed;
+        }
+        else if (tapButton == "Dash Backward")
+        {
+            dashVector = -transform.forward * dashSpeed;
+        }
+
+        Invoke(nameof(ResetDash), dashCooldown);
+        return dashVector;
+    }
+
+    private void CalculateVelocity()
+    {
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        Vector3 dash = Vector3.zero;
+        if (readyToDash && dashing)
+        {
+            dash = Dash();
+        }
+
+        // Apply Velocity to the rigid body while also handling collisions
+        Vector3 direction = (transform.forward * y) + (transform.right * x);
+        velocity = (direction * speed * Time.fixedDeltaTime) + dash;
+    }
+
+    private void ResetDash()
+    {
+        readyToDash = true;
     }
     //-----------------------------------------------------------------------------------//
 
